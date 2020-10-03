7ba9ddf31e76fdac7d2a78597cbb398ba830cdb4
dmkolosov <maker.bug.00@inbox.ru>
2020-10-03 17:16:08 +0300

    feat: add min function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array=[]) {
+  let min=array.length>0?array[0]:0
+  for (let index = 0; index < array.length; index++) {
+    if(array[index]<min){
+      min=array[index]
+    }
+    
+  }
+  return min;
 }
 
 exports.max = function max (array) {
