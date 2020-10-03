c6fa69ca199164d9b8f286bedcabf80f7202daa2
dmkolosov <maker.bug.00@inbox.ru>
2020-10-03 17:16:42 +0300

    feat: add max function

--- src/index.js
+++ src/index.js
@@ -10,8 +10,15 @@ exports.min = function min (array=[]) {
   return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array=[]) {
+  let max=array.length>0?array[0]:0
+  for (let index = 0; index < array.length; index++) {
+    if(array[index]>max){
+      max=array[index]
+    }
+    
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
